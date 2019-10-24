c5b145770bcb9fd11af6a6eabdb59f17c55fed85
Zhonghua Bao <zbao4@stevens.edu>
2019-10-24 12:59:28 -0400

    feature of mocking GitHub API

--- Homework04.py
+++ Homework04.py
@@ -1,11 +1,12 @@
 import json
 import requests
 
+
 def get_repo_info(user_name='Rosovski'):
     output = []
     user_url = 'https://api.github.com/users/{}/repos'.format(user_name)
     res = requests.get(user_url)
-    repos = json.loads(res.text)
+    repos = res.json()
     output.append('User: {}'.format(user_name))
 
     try:
@@ -16,15 +17,16 @@ def get_repo_info(user_name='Rosovski'):
     try:
         for repo in repos:
             repo_name = repo['name']
-            repo_url = 'https://api.github.com/repos/{}/{}/commits'.format(user_name, repo_name)
+            repo_url = 'https://api.github.com/repos/{}/{}/commits'.format(
+                user_name, repo_name)
             repo_info = requests.get(repo_url)
             repo_info_json = json.loads(repo_info.text)
-            output.append('Repo: {} Number of commits: {}'.format(repo_name, len(repo_info_json)))
+            output.append('Repo: {} Number of commits: {}'.format(
+                repo_name, len(repo_info_json)))
     except (TypeError, KeyError, IndexError):
         return 'unable to fetch commits from repo'
-    
-    return output
 
+    return output
 
 
 if __name__ == '__main__':
---
+++ Test_new_HW04.py
@@ -0,0 +1,32 @@
+import unittest
+from unittest import mock
+import json
+import new_HW04
+import requests
+import json
+
+
+def fake_get(url):
+    if url.startswith("https://api.github.com/users/"):
+        return open("getReposResponse.json").read()
+    elif url.startswith("https://api.github.com/repos/"):
+        url = url[29:]  # remove the above prefix
+        url = url.replace("/commits", "")  # remove /commits
+        repo_name = url[url.find("/") + 1:]  # remove username
+        # read the file content from the associated json file
+        return open("repo-" + repo_name + ".json").read()
+
+
+class TestHW4(unittest.TestCase):
+
+    @mock.patch("requests.get", side_effect=fake_get)
+    def test_something(self, mocked):
+        content = requests.get("https://api.github.com/repos/Rosovski/Triangle567/commits")
+        expect = 5
+        self.assertEquals(expect, len(json.loads(content)))
+        # for line in json.loads(content):
+        #     print(line["name"])
+
+    
+if __name__ == '__main__':
+    unittest.main()
